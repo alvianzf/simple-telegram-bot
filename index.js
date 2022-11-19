c20eeba251e2ad619d14e756c1330540df0619ff
alvianzf <alvianzf@gmail.com>
2022-11-19 22:11:43 +0700

    add share location and contact functionalities

--- index.js
+++ index.js
@@ -7,7 +7,52 @@ const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
 bot.command('start', ctx => {
     console.log(ctx.from);
-    bot.telegram.sendMessage(ctx.chat.id, "Hello World!");
+    bot.telegram.sendMessage(ctx.chat.id, "Which function would you like to do?", {
+        reply_markup: {
+            inline_keyboard: [
+            [{
+                text: "I want to know my location",
+                callback_data: "location"
+            },
+            {
+                text: "What is my phone number",
+                callback_data: "phone"
+            }]
+            ]
+        }
+    });
+});
+
+bot.action('location', ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, "Rly?", {
+        "reply_markup": {
+            "one_time_keyboard": true,
+            "keyboard": [
+                [{
+                    text: "Show my location",
+                    request_location: true,
+                    one_time_keyboard: true
+                }],
+                ["Cancel"]
+            ]
+        }
+    });
+});
+
+bot.action('phone', ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, "Send my contact?", {
+        "reply_markup": {
+            "one_time_keyboard": true,
+            "keyboard": [
+                [{
+                    text: "Share my contact",
+                    request_contact: true,
+                    one_time_keyboard: true
+                }],
+                ["Cancel"]
+            ]
+        }
+    });
 });
 
 bot.launch();
